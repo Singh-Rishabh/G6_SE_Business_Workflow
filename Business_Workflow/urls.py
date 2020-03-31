92850b61ac4d6971f47b951cf024ac268ed98ac2
Siddharth Nahar <2016csb1043@iitrpr.ac.in>
2020-04-01 03:54:18 +0530

    Just some refinements for forms

--- Business_Workflow/urls.py
+++ Business_Workflow/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('forms/', include('forms.urls')),
 	path('schema/', include('schema.urls')),
     path('admin/', admin.site.urls),
 ]
