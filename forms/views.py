cfffc4c994aff9e35b38bbce376beb66f10665c5
Siddharth Nahar <2016csb1043@iitrpr.ac.in>
2020-04-07 20:03:08 +0530

    Refactor , Modularize Templates for efficiency

--- forms/views.py
+++ forms/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+form_field_types = {
+
+	'text' : 'text',
+	'textarea' : 'textarea',
+	'email' : 'email',
+	'number' : 'number',
+	'date' : 'date',
+	'radio' : 'radio',
+	'checkbox' : 'checkbox'
+
+}
+
 def index(request):
     return HttpResponse("Hello, world. You're at the forms index.")
 
@@ -9,4 +21,4 @@ def home(request):
 	return render(request , 'forms/index.html')
 
 def createform(request):
-	return render(request , 'forms/createform.html')
+	return render(request , 'forms/createform.html', {'form_field_types' : form_field_types})
